572328e8dd9f31d1696ce5366337ec9694d1ea7e
Marcelo-Deposito <smouraconstrucoes@gmail.com>
2024-08-27 10:34:11 -0300

    Criando um Sistema Bancário com Python

--- 00 - Fundamentos/desafio.py
+++ 00 - Fundamentos/desafio.py
@@ -16,51 +16,56 @@ LIMITE_SAQUES = 3
 while True:
 
     opcao = input(menu)
-
-    if opcao == "d":
-        valor = float(input("Informe o valor do depósito: "))
-
-        if valor > 0:
-            saldo += valor
-            extrato += f"Depósito: R$ {valor:.2f}\n"
-
-        else:
-            print("Operação falhou! O valor informado é inválido.")
-
-    elif opcao == "s":
-        valor = float(input("Informe o valor do saque: "))
-
-        excedeu_saldo = valor > saldo
-
-        excedeu_limite = valor > limite
-
-        excedeu_saques = numero_saques >= LIMITE_SAQUES
-
-        if excedeu_saldo:
-            print("Operação falhou! Você não tem saldo suficiente.")
-
-        elif excedeu_limite:
-            print("Operação falhou! O valor do saque excede o limite.")
-
-        elif excedeu_saques:
-            print("Operação falhou! Número máximo de saques excedido.")
-
-        elif valor > 0:
-            saldo -= valor
-            extrato += f"Saque: R$ {valor:.2f}\n"
-            numero_saques += 1
-
-        else:
-            print("Operação falhou! O valor informado é inválido.")
-
-    elif opcao == "e":
-        print("\n================ EXTRATO ================")
-        print("Não foram realizadas movimentações." if not extrato else extrato)
-        print(f"\nSaldo: R$ {saldo:.2f}")
-        print("==========================================")
-
-    elif opcao == "q":
-        break
-
-    else:
-        print("Operação inválida, por favor selecione novamente a operação desejada.")
+    match opcao:
+        case "q":
+            print("Saindo do sistema...")
+            break
+        case "d":
+            try:
+                valor = float(input("Qual valor você deseja depositar: R$"))
+                if valor<=0:
+                    print("Valor inválido! Digite um número positivo!")
+                    continue
+                saldo+=valor
+                print("Depósito efetuado com sucesso!")
+                print(f"Saldo atual de R${saldo:.2f}")
+                extrato += f"Depósito: R${valor:.2f}\n"
+            except ValueError:
+                print("Valor inválido! Tente novamente!")
+                continue
+        case "s":
+            try:
+                if saldo==0:
+                    print("Você não possui saldo para sacar.")
+                    continue
+                if numero_saques >= LIMITE_SAQUES:
+                    print("Você atingiu o limite de saques!")
+                    continue
+                print(f"Você possui {LIMITE_SAQUES - numero_saques} saques restantes de no máximo R$500.00 cada")
+                valor_saque = float(input("Qual valor você deseja depositar: R$"))
+                if valor_saque <=0:
+                    print("Valor inválido! Digite um número positivo!")
+                    continue
+                if valor_saque > 500:
+                    print("Valor do saque excede o limite!")
+                    continue
+                if saldo < valor_saque:
+                    print("Saldo insuficiente!")
+                    continue
+                saldo -= valor_saque
+                numero_saques += 1
+                print("Saque efetuado com sucesso!")
+                print(f"Saldo atual de R${saldo:.2f}")
+                extrato += f"Saque: R${valor_saque:.2f}\n"
+            except ValueError:
+                print("Valor inválido! Tente novamente!")
+                continue
+        case "e":
+            if not extrato:
+                print("Você não realizou nenhuma operação.")
+            else:
+                print("Extrato:")
+                print(extrato,end="")
+                print(f"Seu saldo atual é de R${saldo:.2f}")
+        case _:
+            print("Opção invalida, tente novamente.")
